489c3bb920798ca4f0a7187b007cc665c325a4b4
Alberto Forni <me@albertoforni.com>
2021-02-11 09:40:16 +0100

    Add example with .then

--- src/06-order-of-execution.ts
+++ src/06-order-of-execution.ts
@@ -1,10 +1,46 @@
-console.log(Promise.resolve("1"));
+// no async execution
+new Promise((resolve) => {
+  console.log("1-inPromise");
+  resolve(undefined);
+});
 console.log("1");
-console.log(Promise.resolve("2"));
+new Promise((resolve) => {
+  console.log("2-inPromise");
+  resolve(undefined);
+});
 console.log("2");
-console.log(Promise.resolve("3"));
+new Promise((resolve) => {
+  console.log("3-inPromise");
+  resolve(undefined);
+});
 console.log("3");
 
+// // ------
+// // async execution with then
+// new Promise((resolve) => {
+//   console.log("1-inPromise");
+//   resolve(undefined);
+// }).then(() => {
+//   console.log("1-inPromise-then");
+// });
+// console.log("1");
+// new Promise((resolve) => {
+//   console.log("2-inPromise");
+//   resolve(undefined);
+// }).then(() => {
+//   console.log("2-inPromise-then");
+// });
+// console.log("2");
+// new Promise((resolve) => {
+//   console.log("3-inPromise");
+//   resolve(undefined);
+// }).then(() => {
+//   console.log("3-inPromise-then");
+// });
+// console.log("3");
+
+// // ------
+// // async execution with then, with Promise.resolve
 // console.log(
 //   Promise.resolve("1").then((val) => {
 //     console.log(val + "-1");
